7ddcd2f3883cf2e980c407935ce78052448286b8
foostan <ks@fstn.jp>
2018-02-11 16:48:00 +0900

    Remove mouse, Add a ESC line in LOWER and RAISE

--- keyboards/lets_split/keymaps/foostan/keymap.c
+++ keyboards/lets_split/keymaps/foostan/keymap.c
@@ -42,31 +42,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
        KC_LSFT,       KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,          KC_N,       KC_M,    KC_COMM,     KC_DOT,    KC_SLSH,    KC_TAB,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-       KC_MS_L,    KC_MS_D,    KC_BTN1,    KC_LALT,      LOWER,     KC_SPC,        KC_ENT,      RAISE,    KC_RALT,    KC_BTN2,    KC_MS_U,   KC_MS_R \
+       XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_LALT,      LOWER,     KC_SPC,        KC_ENT,      RAISE,    KC_RALT,    XXXXXXX,    XXXXXXX,   XXXXXXX \
 //`-----------------------------------------------------------------------'  `-----------------------------------------------------------------------'
 ),
 
 [_LOWER] = KEYMAP( \
 //,-----------------------------------------------------------------------.  ,-----------------------------------------------------------------------.
-          KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,          KC_7,       KC_8,       KC_9,       KC_0,    XXXXXXX,   KC_BSPC,\
+        KC_ESC,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,          KC_6,       KC_7,       KC_8,       KC_9,       KC_0,   KC_BSPC,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-         KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,         KC_F7,      KC_F8,      KC_F9,     KC_F10,    XXXXXXX,   XXXXXXX,\
+      KC_LCTRL,      KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,         KC_F6,      KC_F7,      KC_F8,      KC_F9,     KC_F10,   XXXXXXX,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-        KC_F11,     KC_F12,     KC_F13,     KC_F14,     KC_F15,     KC_F16,        KC_F17,     KC_F18,     KC_F19,     KC_F20,    XXXXXXX,   XXXXXXX,\
+       KC_LSFT,     KC_F11,     KC_F12,     KC_F13,     KC_F14,     KC_F15,        KC_F16,     KC_F17,     KC_F18,     KC_F19,     KC_F20,   XXXXXXX,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-       KC_MS_L,    KC_MS_D,    KC_BTN1,    KC_LALT,      LOWER,     KC_SPC,        KC_ENT,      RAISE,    KC_RALT,    KC_BTN2,    KC_MS_U,   KC_MS_R \
+       XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_LALT,      LOWER,     KC_SPC,       KC_BSPC,      RAISE,     KC_TAB,    XXXXXXX,    XXXXXXX,   XXXXXXX \
 //`-----------------------------------------------------------------------'  `-----------------------------------------------------------------------'
 ),
 
 [_RAISE] = KEYMAP( \
 //,-----------------------------------------------------------------------.  ,-----------------------------------------------------------------------.
-       KC_EXLM,      KC_AT,    KC_HASH,     KC_DLR,    KC_PERC,    KC_CIRC,       KC_AMPR,    KC_ASTR,   KC_LPRN,     KC_RPRN,  S(KC_GRV),   KC_BSPC,\
+        KC_ESC,    KC_EXLM,      KC_AT,    KC_HASH,     KC_DLR,    KC_PERC,       KC_CIRC,    KC_AMPR,    KC_ASTR,   KC_LPRN,     KC_RPRN,   KC_BSPC,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,       KC_MINS,     KC_EQL, S(KC_LBRC), S(KC_RBRC), S(KC_BSLS),    KC_GRV,\
+      KC_LCTRL,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,       KC_MINS,     KC_EQL, S(KC_LBRC), S(KC_RBRC), S(KC_BSLS),    KC_GRV,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    S(KC_MINS),  S(KC_EQL),    KC_LBRC,    KC_RBRC,    KC_BSLS,   XXXXXXX,\
+       KC_LSFT,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    S(KC_MINS),  S(KC_EQL),    KC_LBRC,    KC_RBRC,    KC_BSLS, S(KC_GRV),\
 //|-----------+-----------+-----------+-----------+-----------+-----------|  |-----------+-----------+-----------+-----------+-----------+-----------|
-       KC_MS_L,    KC_MS_D,    KC_BTN1,    KC_LALT,      LOWER,     KC_SPC,        KC_ENT,      RAISE,    KC_RALT,    KC_BTN2,    KC_MS_U,   KC_MS_R \
+       XXXXXXX,    XXXXXXX,    XXXXXXX,     KC_TAB,      LOWER,     KC_ESC,        KC_ENT,      RAISE,    KC_RALT,    XXXXXXX,    XXXXXXX,   XXXXXXX \
 //`-----------------------------------------------------------------------'  `-----------------------------------------------------------------------'
 ),
 
