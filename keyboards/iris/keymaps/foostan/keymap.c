34eff9ae5edacb46b1a6aa719109faaff2fd4ec6
foostan <ks@fstn.jp>
2018-04-13 02:46:39 +0900

    Update Iris

--- keyboards/iris/keymaps/foostan/keymap.c
+++ keyboards/iris/keymaps/foostan/keymap.c
@@ -65,19 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //|-----------+-----------+-----------+-----------+-----------+-----------+-----------.   ,-----------+-----------+-----------+-----------+-----------+-----------+-----------|
        KC_LSFT,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX, S(KC_MINS),  S(KC_EQL),    KC_LBRC,    KC_RBRC,    KC_BSLS, S(KC_GRV),\
 //|-----------+-----------+-----------+-----------+-----------+-----------|-----------/   \-----------+-----------+-----------+-----------+-----------+-----------+-----------|
-                                                 KC_TAB,      LOWER,     KC_ESC,                       KC_ENT,      RAISE,    KC_RGUI
-//                                         `-----------+-----------+-----------'                 `-----------+-----------+-----------'
+                                              KC_TAB,      LOWER,     KC_ESC,                       KC_ENT,      RAISE,    KC_RGUI
+//                                      `-----------+-----------+-----------'                 `-----------+-----------+-----------'
   ),
 
   [_ADJUST] = KEYMAP(
 //,-----------+-----------+-----------+-----------+-----------+-----------.                           ,-----------+-----------+-----------+-----------+-----------+-----------.
-        RESET ,     DEBUG ,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
+         RESET,     DEBUG ,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|                           |-----------+-----------+-----------+-----------+-----------+-----------|
-       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
+       RGB_TOG,    RGB_HUI,    RGB_SAI,    RGB_VAI,    XXXXXXX,    XXXXXXX,                                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|                           |-----------+-----------+-----------+-----------+-----------+-----------|
-       XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
+      RGB_SMOD,    RGB_HUD,    RGB_SAD,    RGB_VAD,    XXXXXXX,    XXXXXXX,                                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
 //|-----------+-----------+-----------+-----------+-----------+-----------+-----------.   ,-----------+-----------+-----------+-----------+-----------+-----------+-----------|
-      BL_STEP ,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
+       BL_STEP,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,\
 //|-----------+-----------+-----------+-----------+-----------+-----------|-----------/   \-----------+-----------+-----------+-----------+-----------+-----------+-----------|
                                                 KC_LGUI,      LOWER,     KC_SPC,                       KC_ENT,      RAISE,    KC_RGUI
 //                                         `-----------+-----------+-----------'                 `-----------+-----------+-----------'
