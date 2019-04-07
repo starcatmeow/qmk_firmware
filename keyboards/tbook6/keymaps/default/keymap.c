070409aac56589037bd600b1167b063a75ebf8a5
foostan <ks@fstn.jp>
2019-04-08 02:57:21 +0900

    Fix for matrix

--- keyboards/tbook6/keymaps/default/keymap.c
+++ keyboards/tbook6/keymaps/default/keymap.c
@@ -25,7 +25,8 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
-    RGB_TOG, RGB_MOD, RGB_HUI  \
+    KC_A, KC_B, \
+    KC_C, KC_D \
   ),
 };
 
